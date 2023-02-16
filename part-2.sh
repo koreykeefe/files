d580bf44729714e6eca57fe27ba0ce7927bb129f
Korey Keefe <88954147+koreykeefe@users.noreply.github.com>
2023-02-16 06:00:20 -0500

    Update part-2.sh

--- part-2.sh
+++ part-2.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 sudo apt install -y autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
